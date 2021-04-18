0dcf64034caf6dd84bb10e24a2dcd9f2f073cc90
Jason <j_schubb53@yahoo.com>
2021-04-18 19:50:04 -0400

    first commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleObjects
+{
+    class Circle
+    { 
+        //fields
+        private double _radius;
+        
+        //constructor
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public Circle(double radius)
+        {   
+            this._radius = radius;
+        }
+
+        //methods
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+
+        public string CalculateFormattedCircumference(double circumference)
+        {
+            string circumferenceF = FormatNumber(circumference);
+            return circumferenceF;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        public string CalculateFormattedArea(double area)
+        {
+            string areaF = FormatNumber(area);
+            return areaF;
+        }
+
+        private string FormatNumber(double x)
+        {
+            x = Math.Round (x,2);
+            string x1 = x.ToString();
+            return x1;
+        }
+
+    }
+    
+}
+
+
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CircleObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Circle circle1 = new Circle(0);
+
+            double circumference = 0;
+            double area = 0;
+            string circumferenceF = "";
+            string areaF = "";
+
+            int count = 0;
+
+            bool runProgram = true;
+            while (runProgram)
+               
+            try
+                {
+                    Console.WriteLine("Please enter a radius to be calculated");
+                    double radius = double.Parse(Console.ReadLine());
+                    if (radius < 0)
+                    {
+                        throw new Exception("Cannot be less than 0");
+                    }
+                    else
+                    {
+                        count += 1;
+
+                        circumference = circle1.CalculateCircumference(radius);
+                        circumferenceF = circle1.CalculateFormattedCircumference(circumference);
+                        Console.WriteLine($"Formatted circumference of the circle is: {circumferenceF}");
+                        area = circle1.CalculateArea(radius)
+                        areaF = circle1.CalculateFormattedArea(area);
+                        Console.WriteLine($"Formatted area is : {areaF}");
+
+                        Console.WriteLine("Do you want to continue? y/n)");
+                        
+                        
+
+                        while (true)
+                        {
+                            string yesno = Console.ReadLine();
+                                             
+                            if (yesno == "y")
+                            {
+                                break;
+                            }
+                            else if (yesno == "n")
+                            {
+                                Console.WriteLine($"Goodbye.  You created {count} circles.");
+                                runProgram = false;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("That was not a valid entry.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+            }            
+        }
+    }
+}
+
+
+
+
+
